f4616604ebe84f88506e270add2a8eb9e2cc23b5
adonay2003 <gerson.melara22@itca.edu.sv>
2023-10-22 22:41:13 -0500

    Create commit_3.dart
    commit3

---
+++ lib/pages/commit 2/commit_3.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class adonay extends StatelessWidget {
+  const adonay({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
