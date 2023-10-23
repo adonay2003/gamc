340cbfb2a3ced7e1ab7cb3929e4d974b71e2d062
adonay2003 <gerson.melara22@itca.edu.sv>
2023-10-22 22:42:54 -0500

    Create commit_5.dart
    commit 5

---
+++ lib/pages/commit 2/commit_5.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class campos extends StatelessWidget {
+  const campos({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
